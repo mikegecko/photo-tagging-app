946918824114b7a26cb4e6dc63da73aaf4cecb39
Michael Guedko <mjgeko@gmail.com>
2023-01-02 18:24:16 -0500

    Add comments

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ import Sidebar from './components/Sidebar';
 import Start from './routes/Start';
 import PhotoPage from './routes/PhotoPage'
 
+/* 
+---------- TODO ----------
+1. Setup Firebase Database
+  - Figure out structure of user data
+  - Authentication / Session token ?
+  - Image storage
+2. Finish designing UI
+  - Sidebar design finalization
+    * Sidebar Items get crossed out when selection is a match
+  - Start Page design
+    * Add background image
+  - Leaderboard Page creation
+3. Image search
+  - Find images to use for searching
+  - Design Tagging system
+    * Animate click that extends a line from click point
+      then opens a selection dialogue
+*/
+
 function App() {
   return (
     <div className="App">
