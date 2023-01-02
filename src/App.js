15ecf6ee8b88ec7f5207d78e7535f89219394a6f
Michael Guedko <mjgeko@gmail.com>
2023-01-02 18:17:03 -0500

    Implement routes and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Start from './routes/Start';
+import PhotoPage from './routes/PhotoPage'
 
 function App() {
   return (
     <div className="App">
-      Loaded
+      <BrowserRouter>
+      <Sidebar />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/lvl1' element={<PhotoPage />} />
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
