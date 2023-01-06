4690d29bf15d2fbf9c2cd48f188dd3ab48071bea
Michael Guedko <mjgeko@gmail.com>
2023-01-06 16:55:28 -0500

    Fix sidebar overflow

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ export default function Sidebar(props){
         <Drawer anchor="left" variant="permanent" elevation={1}>
             <div className="drawer">
                 <h3>Items</h3>
-                <List>
+                <List sx={{backgroundColor: 'rgb(30,30,30)'}}>
                 {props.loading ? <div>...loading</div> : props.items.items.map((item, index) => {
                 return(<ListItem disabled={item.isFound} disablePadding key={index}><ListItemText sx={{textAlign: 'center'}} primary={item.name} /></ListItem>)
             })}
