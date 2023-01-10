b13cc3fc2017cfbe528310f8048cc16421f56779
Michael Guedko <mjgeko@gmail.com>
2023-01-10 14:31:35 -0500

    Add table styling and show only top 10 scores
        - sort function for usersData, sorts based on time

--- src/routes/LeaderboardPage.jsx
+++ src/routes/LeaderboardPage.jsx
@@ -6,7 +6,7 @@ import { collection, getDocs } from "firebase/firestore";
 export default function LeaderboardPage(props) {
     const [usersData, setUsersData] = useState([]);
     const [loading, setLoading] = useState(false);
-
+    let c = 10;
     useEffect(() => {
         setUsersData([]);
         setLoading(true);
@@ -24,13 +24,19 @@ export default function LeaderboardPage(props) {
         getUsersData();
 
     }, [])
+
+    const sortUsers = () => {
+        let sorted = usersData.sort(
+            (u1,u2) => (u1.time > u2.time) ? 1 : (u1.time < u2.time) ? -1 : 0);
+        return(sorted);
+    }
     return(
         <div className="overlay scroll">
-            <Box sx={{overflow:'auto'}}>
+            <Box sx={{overflow:'auto', width: 200}}>
                 <h3>Leaderboard</h3>
                 <Divider />
                 <TableContainer>
-                    <Table>
+                    <Table sx={{width: 200}}>
                         <TableHead>
                             <TableRow>
                                 <TableCell>Name</TableCell>
@@ -38,23 +44,28 @@ export default function LeaderboardPage(props) {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {loading ? <div>...loading</div> : usersData.map((u,index) => {
-                                if(!u.time){
-                                    return;
-                                }
-                                else{
-                                    return(
-                                        <TableRow key={index} >
-                                        <TableCell>{u.name}</TableCell>
-                                        <TableCell>{u.time}</TableCell>
-                                        </TableRow>
-                                    )
+                            {loading ? <TableRow><TableCell>...loading</TableCell></TableRow> : sortUsers().map((u,index) => {
+                                
+                                if(index < c ){
+                                    if(!u.time){
+                                        c++;
+                                        return;
+                                    }
+                                    else{
+                                        return(
+                                            <TableRow key={index} >
+                                            <TableCell>{u.name}</TableCell>
+                                            <TableCell align="right">{u.time}</TableCell>
+                                            </TableRow>
+                                        )
+                                    }
                                 }
                             })
                             }
                         </TableBody>
                     </Table>
                 </TableContainer>
+                <Divider />
                 <Box>
                     <div>Name: {props.user.name}</div>
                     <div>Time: {props.time}</div>
